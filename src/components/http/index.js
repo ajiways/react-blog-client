99f56c30e91498b57eaa8bb0f8420adbf73ad4e6
ajiways <ajiwayswc@gmail.com>
2022-03-19 13:20:01 +0700

    changed api_url to heroku

--- src/components/http/index.js
+++ src/components/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = `http://localhost:4999`;
+export const API_URL = `https://kswbtw-blog-backend.herokuapp.com/`;
 
 const $api = axios.create({
    withCredentials: true,
